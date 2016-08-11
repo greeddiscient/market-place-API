f37089439214a59ae8517c8d1125e3a14c04f6bd
Shaun Djuhari <shaundjuhari@berkeley.edu>
2016-08-11 22:11:27 +0700

    finishes modeling the product model along with user associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,8 @@ describe User do
 
   subject { @user }
 
+  it { should have_many(:products) }
+
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
@@ -12,7 +14,7 @@ describe User do
 
   it { should be_valid }
   it { should validate_uniqueness_of(:auth_token)}
-    describe "#generate_authentication_token!" do
+  describe "#generate_authentication_token!" do
     it "generates a unique token" do
       Devise.stub(:friendly_token).and_return("auniquetoken123")
       @user.generate_authentication_token!
@@ -23,6 +25,21 @@ describe User do
       existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
       @user.generate_authentication_token!
       expect(@user.auth_token).not_to eql existing_user.auth_token
+    end
+  end
+  describe "#products association" do
+
+    before do
+      @user.save
+      3.times { FactoryGirl.create :product, user: @user }
+    end
+
+    it "destroys the associated products on self destruct" do
+      products = @user.products
+      @user.destroy
+      products.each do |product|
+        expect(Product.find(product)).to raise_error ActiveRecord::RecordNotFound
+      end
     end
   end
 end
